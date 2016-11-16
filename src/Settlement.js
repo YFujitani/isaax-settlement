8993048d9c08522ff0585241e647cca2fdc72a4a
fujitani <tiny00062000@gmail.com>
2016-11-17 01:27:27 +0900

    node -> shell呼び出し

--- src/Settlement.js
+++ src/Settlement.js
@@ -1,7 +1,17 @@
 "use strict";
+import shell from "./shellHelper";
 
 export default class Settlement {
   constructor() {
     console.log('Settlement#constructor is called')
+  }
+  generate() {
+    console.log('Settlement#generate is called')
+  }
+  nativeCall() {
+    console.log('Settlement#nativeCall is called')
+    shell.exec('ls -la', function(err){
+      console.log('executed test');
+    });
   }
 }
